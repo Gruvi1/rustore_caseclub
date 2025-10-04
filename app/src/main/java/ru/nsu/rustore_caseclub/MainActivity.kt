1bfe1c94bb8eddd1cb68362126a1b99cf8122d08
orangetee1 <zhusalin519@gmail.com>
2025-10-04 21:34:44 +0700

    update

--- app/src/main/java/ru/nsu/rustore_caseclub/MainActivity.kt
+++ app/src/main/java/ru/nsu/rustore_caseclub/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ru.nsu.rustore_caseclub.model.AppInfo
+import ru.nsu.rustore_caseclub.ui.AppCard
 import ru.nsu.rustore_caseclub.ui.theme.Rustore_caseclubTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +22,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             Rustore_caseclubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    println(innerPadding)
+
+                    val vkAppInfo = AppInfo(
+                        id = 1,
+                        name = "VK",
+                        category = "Социальные сети",
+                        description = "VK — это социальная сеть для общения, обмена медиа и доступа к музыке, видео и сообществам.",
+                        icon = "file:///android_asset/vk_icon.png",
+                        screenshots = listOf(
+                            "https://www.rustore.ru/help/assets/images/download-f690244ac26c7242a94e303d8a03796e.webp",
+                            "https://www.rustore.ru/help/assets/images/description-fa26cd4dd77d390831208550893078c6.webp"
+                        ),
+                        developerCompany = "VK Devs",
+                        ageRating = "12+"
                     )
+
+                    AppCard(null, vkAppInfo)
                 }
             }
         }
